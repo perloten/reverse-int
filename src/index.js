375185a8f18e52f4128c20f55580010f3f62bdbf
Кириченко Евгения <ekirichenko@rapidsoft.ru>
2020-10-07 00:21:03 +0300

    feat: add solution for task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+if (typeof n !== 'number') {
+    throw new Error ('Wrong argument!');
+}
+  return Number(Math.abs(n).toString().split('').reverse().join(''));
 }
